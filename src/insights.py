077674fd5a9af8961831ccd02b0cadd5bcce418c
Juliana Giusti <juliana.vmelo@gmail.com>
2022-10-21 00:05:47 -0300

    feat: get_unique_industries function

--- src/insights.py
+++ src/insights.py
@@ -56,7 +56,11 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    job_data_dict = read(path)
+    industries_list = {
+        job["industry"] for job in job_data_dict if len(job["industry"]) > 0
+    }
+    return list(industries_list)
 
 
 def filter_by_industry(jobs, industry):
