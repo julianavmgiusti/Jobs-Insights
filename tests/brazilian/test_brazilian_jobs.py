0deca6b09473f4f3c0add755014c3370247897e5
Juliana Giusti <juliana.vmelo@gmail.com>
2022-10-23 01:52:45 -0300

    test: adding test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,14 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+path = "tests/mocks/brazilians_jobs.csv"
+
+
+titles_mock = {
+    "title": "Analista de Software",
+    "salary": "4000",
+    "type": "full time"
+    }
 
 
 def test_brazilian_jobs():
-    pass
+    translation = read_brazilian_file(path)
+    assert titles_mock in translation
