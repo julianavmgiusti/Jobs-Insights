85696cbdccafa1563941c25e643961f65b5af128
Juliana Giusti <juliana.vmelo@gmail.com>
2022-10-23 10:18:49 -0300

    test: adding test_sorting

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,31 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+
+jobs_info = [
+    {
+        "min_salary": 5000,
+        "max_salary": 6000,
+        "date_posted": "2022-03-22",
+    },
+    {
+        "min_salary": 2800,
+        "max_salary": 5000,
+        "date_posted": "2022-04-30",
+    },
+    {
+        "min_salary": 1000,
+        "max_salary": 2000,
+        "date_posted": "2022-10-17",
+    },
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs_info, "min_salary")
+    assert jobs_info[0]["min_salary"] == 1000
+
+    sort_by(jobs_info, "max_salary")
+    assert jobs_info[0]["max_salary"] == 6000
+
+    sort_by(jobs_info, "date_posted")
+    assert jobs_info[0]["date_posted"] == "2022-10-17"
